6aa7646d4f57c2427cb8960f80d00e658fb23962
Greg Larsen <greg.larsen@hpe.com>
2016-01-12 10:07:05 +0100

    updates for split repo

--- jenkins/buildpdf.sh
+++ jenkins/buildpdf.sh
@@ -8,7 +8,7 @@ export JAVA_HOME="/usr/lib/jvm/java-1.7.0-openjdk-amd64"
 
 #rm -r tools
 #git clone git@github.com:hphelion/tools.git 
-#cd tools/DITA-OT
+cd tools/DITA-OT
 #git checkout pdf_test
  
 export DITA_HOME="`pwd`"
